809b22fe35f4a46ae15fe44292e82590f690b78a
Yan <podoyo23@gmail.com>
2025-01-16 00:53:35 +0800

    add:練習-2sum-暴力版

---
+++ Two Sum/暴力版解法/twosum.js
@@ -0,0 +1,15 @@
+const num = [2, 7, 11, 15]
+const target = 9
+
+const twoSum = (num, target) => {
+	for (let i = 0; i < num.length; i++) {
+		for (let j = i + 1; j < num.length; j++) {
+			if (num[i] + num[j] === target) {
+				return [i, j]
+			}
+		}
+	}
+	return []
+}
+
+console.log(twoSum(num, target))
