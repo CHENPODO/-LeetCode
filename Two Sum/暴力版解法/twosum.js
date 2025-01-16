5b49283e91cfe9b710c4f250abaf150a2d38b81c
Yan <podoyo23@gmail.com>
2025-01-16 11:53:51 +0800

    Update:練習-2sum-暴力版-加入註解跟console測試其他可能

--- Two Sum/暴力版解法/twosum.js
+++ Two Sum/暴力版解法/twosum.js
@@ -2,14 +2,19 @@ const num = [2, 7, 11, 15]
 const target = 9
 
 const twoSum = (num, target) => {
+	// 外層迴圈選擇第一個數
 	for (let i = 0; i < num.length; i++) {
+		// 內層迴圈選擇第二個數
 		for (let j = i + 1; j < num.length; j++) {
 			if (num[i] + num[j] === target) {
-				return [i, j]
+				return [i, j] // 如果前後兩數相加等於目標值，則返回索引值
 			}
 		}
 	}
-	return []
+	return [] // 如果都不等於目標值，則回傳空陣列
 }
 
-console.log(twoSum(num, target))
+console.log(twoSum(num, target)) // [0, 1]
+console.log(twoSum([2, 7, 11, 15], 9)) // [0, 1]
+console.log(twoSum([1, 3, 4, 2], 6)) // [2, 3]
+console.log(twoSum([3, 3], 6)) // [0, 1]
